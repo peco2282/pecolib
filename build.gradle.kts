66a95bf7580cf2fac59c6af5acfc1ab20798c8e7
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-09-03 13:51:03 +0900

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.kotlin.codegen.optimization.fixStack.removeAlwaysFalseIfeq
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.21"
 //    application
@@ -30,11 +34,9 @@ dependencies {
     implementation("com.google.code.findbugs:jsr305:3.0.2")
 //    // https://mvnrepository.com/artifact/com.google.code.findbugs/annotations
 //    implementation("com.google.code.findbugs:annotations:3.0.1")
-
-
-
-
 }
+
+val buildPath = projectDir.path + "/build"
 
 sourceSets {
     main {
@@ -50,6 +52,53 @@ sourceSets {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.register("sourcesJar", Jar::class.java) {
+    archiveClassifier.set("sources")
+    destinationDirectory.set(file("$buildPath/libs/$version"))
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from(sourceSets.main.get().allSource)
+}
+
+tasks.register("javadocJar", Jar::class.java) {
+    archiveClassifier.set("javadoc")
+    destinationDirectory.set(file("$buildPath/libs/$version"))
+
+    from(tasks.javadoc.get().destinationDir)
+}
+
+tasks.javadoc {
+    include("src/main/java")
+}
+
+tasks.withType(JavaCompile::class.java).configureEach {
+    options.encoding = "UTF-8"
+//    destinationDirectory.set(file("$buildPath/libs/$version"))
+}
+
+tasks.getByName("jar", Jar::class) {
+    destinationDirectory.set(file("$buildPath/libs/$version"))
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().allSource){
+        exclude("**/**.java", "**/**/**.kt")
+    }
+}
+
+tasks.getByName("compileKotlin", KotlinCompile::class) {
+    destinationDirectory.set(file("$buildDir/ktSources"))
+}
+
+tasks.register("archiver") {
+//    destinationDirectory.set(file("$buildPath/libs/$version"))
+    dependsOn(
+        tasks.jar,
+        tasks.getByName("sourcesJar"),
+        tasks.getByName("javadocJar")
+    )
+}
+
+
 
 //kotlin {
 //    jvmToolchain(8)
